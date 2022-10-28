629895376b7e7ac6545b58064fcd21a857eacb1a
Miguel <102910354+MiguelSperle@users.noreply.github.com>
2022-10-28 09:49:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let submit = document.getElementById('ipnS')
+submit.addEventListener('click', (event) => {
+    let input = document.querySelector('#ipn')
+    let transfInput = Number(input.value)
+    let resposta = document.querySelector('#res')
+
+
+    if (input.value == ''){
+       alert('Coloque alguma idade!')
+
+    } 
+
+    if(transfInput > 0 && transfInput <= 15){
+        resposta.innerHTML = 'Não vota!'
+    } else if (transfInput > 15 && transfInput < 18){
+        resposta.innerHTML = 'Voto opcional'
+    } else if (transfInput > 17 && transfInput <= 69){
+        resposta.innerHTML = 'Voto obrigatorio'
+    } else if (transfInput >= 70 && transfInput <= 110){
+        resposta.innerHTML = 'Voto opcional'
+    }
+})
+
+
